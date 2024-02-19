2c6d3e4f8bdc6797c1d5fc36b9aeaf2056481174
Shashankvenigalla <shashankvenigalla@gmail.com>
2024-02-19 20:22:57 +0530

    commited

---
+++ CreateSingleNode.java
@@ -0,0 +1,11 @@
+public class CreateSingleNode {
+        static class Node {
+            int data;
+            Node next;
+    
+            Node(int d) {
+                data = d;
+                next = null;
+            }
+        }
+}
